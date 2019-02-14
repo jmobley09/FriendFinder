33d157cbc7c434d8f795fa0f21308b681bab026f
Joshua Mobley <joshua_mobley@apple.com>
2019-02-13 19:58:26 -0700

    updated finder so that it now functions

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// DATA
+
+var friendsarr = [
+    {
+        "name":"Ahmed",
+        "photo":"https://www.gannett-cdn.com/-mm-/bf328da2ee0b2b24d8e75d3dfc734d51084f17c1/c=60-0-2341-1715/local/-/media/DetroitFreePress/DetroitFreePress/2014/11/08/635510562698110283-Dumb-and-Dumber-2.jpg?width=534&height=401&fit=crop",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+      
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsarr;
+  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,43 +3,23 @@
 const friendData = require("../data/friends");
 
 // routes for the server
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  app.post("/api/friends", function (req, res) {
 
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    // changes scores that are sent in as string into integers for calculations
+    const scores = req.body.scores;
+    if (scores) {
+      for (let i = 0; i < scores.length; i++) {
+        let num = parseInt(scores[i]);
+        scores[i] = num;
+      };
+      friendData.push(req.body);
       res.json(true);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    res.json({ ok: true });
   });
 };
